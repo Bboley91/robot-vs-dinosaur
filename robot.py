8c0c99f77c3f07ef543cb6aa7fd83fb7ada01f13
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-19 15:29:26 -0500

    added attack function for robot

--- robot.py
+++ robot.py
@@ -4,3 +4,5 @@ class Robot:
         self.mecha_ankylosaurus = name
         self.health_points = 100
         self.active_weapon = Weapon
+    def attack(self,dinosaur)-> None:
+        dinosaur.health_points -= self.active_weapon
