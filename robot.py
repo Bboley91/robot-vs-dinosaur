ecf351fe87eff2e82826e315bd580cef9f2a8334
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-19 14:02:02 -0500

    corrected capitaliztion of weapon to Weapon

--- robot.py
+++ robot.py
@@ -3,4 +3,4 @@ class Robot:
     def __init__ (self,name,health_points,active_weapon):
         self.mecha_ankylosaurus = name
         self.health_points = 100
-        self.active_weapon = weapon
+        self.active_weapon = Weapon
