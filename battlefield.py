57050ee7b947b2f9fab08e11786d84c3c3307feb
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-19 15:25:52 -0500

    created battlefield file, imported robot and dinosaur classes, created Battlefield class, started work on Battlefield functions

---
+++ battlefield.py
@@ -0,0 +1,13 @@
+from robot import Robot
+from dinosaur import Dinosaur
+class Battlefield:
+    def __init__ (self):
+        self.location = "A crater"
+
+    def run_game(self) -> None:
+
+    def display_welcome(self) -> None:
+
+    def battle_phase(self) -> None:
+
+    def display_winner(self) -> None:
