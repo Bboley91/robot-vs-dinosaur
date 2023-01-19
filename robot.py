eefea624b7d9a79f4f913ae6687c63890541ef8c
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-19 13:57:45 -0500

    created robot class

---
+++ robot.py
@@ -0,0 +1,6 @@
+class Robot:
+    def __init__ (self,name,health_points,active_weapon):
+        self.mecha_ankylosaurus = name
+        self.health_points = 100
+        self.active_weapon = weapon
+        
