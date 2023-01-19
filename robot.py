16b80e911e985da0b61daf14883d8903536883e8
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-19 14:00:14 -0500

    imported weapon class into robot file

--- robot.py
+++ robot.py
@@ -1,6 +1,6 @@
+from weapon import Weapon
 class Robot:
     def __init__ (self,name,health_points,active_weapon):
         self.mecha_ankylosaurus = name
         self.health_points = 100
         self.active_weapon = weapon
-        
