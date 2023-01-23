1d773859991811389c0b3714f83091c7298faf7e
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-23 17:48:17 -0500

    created run_game function

--- battlefield.py
+++ battlefield.py
@@ -5,11 +5,16 @@ class Battlefield:
         self.location = "A crater"
 
     def run_game(self) -> None:
+        self.display_welcome
+        self.battle_phase
+        self.display_winner
 
     def display_welcome(self) -> None:
-        print("Our match begins with ankylosaurus facing off aginst mecha ankylosaurus!")
+        # print("Our match begins with ankylosaurus facing off aginst mecha ankylosaurus!")
+        # print("The battle begins in",{self.location})
 
     def battle_phase(self) -> None:
+
 
 
     def display_winner(self) -> None:
