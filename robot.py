12679ab2bc98548c9e80710c3f0dacded1564ed3
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-23 18:22:02 -0500

    updated robot class

--- robot.py
+++ robot.py
@@ -1,8 +1,8 @@
 from weapon import Weapon
 class Robot:
-    def __init__ (self,name,health_points,active_weapon):
+    def __init__ (self,name):
         self.mecha_ankylosaurus = name
         self.health_points = 100
-        self.active_weapon = Weapon
+        self.active_weapon = Weapon()
     def attack(self,dinosaur)-> None:
         dinosaur.health_points -= self.active_weapon
