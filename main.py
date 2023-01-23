5d0decac0f6950a9c67f916720948de7521c82f5
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-23 18:47:37 -0500

    updated robot class, and updated main.py file

---
+++ main.py
@@ -0,0 +1,3 @@
+from battlefield import Battlefield
+battlefield_one = Battlefield()
+battlefield_one.run_game
--- robot.py
+++ robot.py
@@ -3,6 +3,6 @@ class Robot:
     def __init__ (self,name):
         self.mecha_ankylosaurus = name
         self.health_points = 100
-        self.active_weapon = Weapon()
+        self.active_weapon = Weapon("Hammer Fist")
     def attack(self,dinosaur)-> None:
         dinosaur.health_points -= self.active_weapon
