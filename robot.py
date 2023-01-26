2076359747178d3f5f278ced38d2e3eb1b9fd612
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-26 12:54:08 -0500

    updated robot class init function

--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ class Robot:
     def __init__ (self,name,health_points):
         self.mecha_ankylosaurus = name
         self.robot_health_points = health_points
-        self.active_weapon = Weapon("Hammer Fist")
+        self.active_weapon = Weapon("Hammer Fist", 25)
     def attack(self,dinosaur)-> None:
         dinosaur.health_points -= self.active_weapon.attack_power
         return dinosaur
