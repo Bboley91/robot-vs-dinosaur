31173b4d73a5c8409dcd07f079c7db77420771c1
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-26 15:55:07 -0500

    updated battlefield display welcome function, battle phase function, display winner function

--- battlefield.py
+++ battlefield.py
@@ -12,22 +12,22 @@ class Battlefield:
         self.display_winner()
 
     def display_welcome(self):
-        print(f"Our match begins with",{self.dinosaur.ankylosaurus},"facing off aginst",{self.robot.mecha_ankylosaurus},"!")
-        print(f"The battle begins in",{self.location})
+        print(f"Our match begins with",self.dinosaur.ankylosaurus,"facing off aginst",self.robot.mecha_ankylosaurus,"!")
+        print(f"The battle begins in",self.location)
 
     def battle_phase(self):
         while (self.dinosaur.dinosaur_health_points >= 0 and self.robot.robot_health_points >= 0):
-            print(f"mecha ankylosaurus' health is at:{self.robot.robot_health_points}")
-            print(f"ankylosaurus' health is at:{self.dinosaur.dinosaur_health_points}")
+            print(f"mecha ankylosaurus' health is at: {self.robot.robot_health_points}")
+            print(f"ankylosaurus' health is at: {self.dinosaur.dinosaur_health_points}")
             print(f"{self.robot.mecha_ankylosaurus} attacked {self.dinosaur.ankylosaurus} with {self.robot.active_weapon.name} doing {self.robot.active_weapon.attack_power} damage!")
             self.robot.attack(self.dinosaur)
             print(f"{self.dinosaur.ankylosaurus} attacked {self.robot.mecha_ankylosaurus} doing {self.dinosaur.dinosaur_attack_power} damage!")
             self.dinosaur.attack(self.robot)
-            if self.dinosaur.dinosaur_health_points != 0:
+            if self.dinosaur.dinosaur_health_points == 0:
                 print(f"Mecha ankylosaurus' health is at: {self.robot.robot_health_points}")
                 print(f"Ankylosaurus' health is at: {self.dinosaur.dinosaur_health_points}")
                 break
-            elif self.robot.robot_health_points != 0:
+            elif self.robot.robot_health_points == 0:
                 print(f"Mecha ankylosaurus' health is at: {self.robot.robot_health_points}")
                 print(f"Ankylosaurus' health is at: {self.dinosaur.dinosaur_health_points}")
                 break
@@ -36,7 +36,7 @@ class Battlefield:
 
     def display_winner(self):
         if self.robot.robot_health_points == 0:
-            print({self.dinosaur}," is the winner!")
+            print(self.dinosaur.ankylosaurus," is the winner!")
         elif self.dinosaur.dinosaur_health_points == 0:
-            print({self.robot}," is the winner!")
+            print(self.robot.mecha_ankylosaurus," is the winner!")
 
