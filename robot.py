9d4ae0664c971343abfdfafb13103fe57535b9d5
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-26 15:10:18 -0500

    updated dinosaur and robot init function to change health points value

--- robot.py
+++ robot.py
@@ -1,9 +1,9 @@
 from weapon import Weapon
 class Robot:
-    def __init__ (self,name,health_points):
+    def __init__ (self,name,robot_health_points):
         self.mecha_ankylosaurus = name
-        self.robot_health_points = health_points
+        self.robot_health_points = robot_health_points
         self.active_weapon = Weapon("Hammer Fist", 25)
-    def attack(self,dinosaur)-> None:
+    def attack(self,dinosaur,)-> None:
         dinosaur.dinosaur_health_points -= self.active_weapon.attack_power
         return dinosaur
