5c2223cffa0e71a6e992efb362fd7fb2514494c1
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-26 15:50:16 -0500

    updatd robot file, update battlefield display welcome function and battlephase function

--- battlefield.py
+++ battlefield.py
@@ -3,7 +3,7 @@ from dinosaur import Dinosaur
 class Battlefield:
     def __init__ (self):
         self.location = "A crater"
-        self.robot = Robot("mecha ankylosaurus",100,25)
+        self.robot = Robot("mecha ankylosaurus",100)
         self.dinosaur = Dinosaur("ankylosaurus",125,20)
 
     def run_game(self):
@@ -19,7 +19,7 @@ class Battlefield:
         while (self.dinosaur.dinosaur_health_points >= 0 and self.robot.robot_health_points >= 0):
             print(f"mecha ankylosaurus' health is at:{self.robot.robot_health_points}")
             print(f"ankylosaurus' health is at:{self.dinosaur.dinosaur_health_points}")
-            print(f"{self.robot.mecha_ankylosaurus} attacked {self.dinosaur.ankylosaurus} with {self.robot.active_weapon} doing {self.robot.active_weapon.attack_power} damage!")
+            print(f"{self.robot.mecha_ankylosaurus} attacked {self.dinosaur.ankylosaurus} with {self.robot.active_weapon.name} doing {self.robot.active_weapon.attack_power} damage!")
             self.robot.attack(self.dinosaur)
             print(f"{self.dinosaur.ankylosaurus} attacked {self.robot.mecha_ankylosaurus} doing {self.dinosaur.dinosaur_attack_power} damage!")
             self.dinosaur.attack(self.robot)
