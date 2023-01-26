487adad5de63132d8d5f9d131c725bfe48bef95c
Brendann Boley <brendannm.boley91@gmail.com>
2023-01-26 12:47:14 -0500

    updated robot class init function and attack function

--- robot.py
+++ robot.py
@@ -1,8 +1,9 @@
 from weapon import Weapon
 class Robot:
-    def __init__ (self,name):
+    def __init__ (self,name,health_points):
         self.mecha_ankylosaurus = name
-        self.health_points = 100
+        self.robot_health_points = health_points
         self.active_weapon = Weapon("Hammer Fist")
     def attack(self,dinosaur)-> None:
-        dinosaur.health_points -= self.active_weapon
+        dinosaur.health_points -= self.active_weapon.attack_power
+        return dinosaur
